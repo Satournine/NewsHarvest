553a8175555efd2a5e08155a2dd0e4fbd76de0a1
Satournine <tunaorkun@outlook.com>
2023-12-03 23:59:27 +0300

    upd

--- DataHarvest/DataHarvest.py
+++ DataHarvest/DataHarvest.py
@@ -4,8 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import datetime
-
+import logging
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
+"""from pymongo import MongoClient"""
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+log_directory = os.path.join(current_directory, "..", "logs")
+os.makedirs(log_directory, exist_ok=True)
+log_filename = "logs.log"
+log_file_path = os.path.join(log_directory, log_filename)
+
+logging.basicConfig(filename=log_file_path, level = logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+"""client = MongoClient("mongodb://localhost:27017")
+db = client["orkun_tuna"]
+
+news_collection = db["news"]
+word_frequency_collection = db["word_frequency"]
+stats_collection = db["stats"]
+"""
 
 chrome_driver_path = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
 browser = webdriver.Chrome()
@@ -18,6 +36,12 @@ fail_count = 0
 browser.get(base_URL)
 
 start_time = time.time()
+
+"""def insert_news(news_data):
+    news_collection.insert_one(news_data)
+
+def insert_stats(stats_data):
+    stats_collection.insert_one(stats_data)"""
 
 def get_links_from_page(page_number):
     page_url = base_URL if page_number == 1 else f"{base_URL}page/{page_number}/"
@@ -55,32 +79,6 @@ def extract_dates(soup, div_class):
 
     return publish_date, update_date
 
-
-"""for news in news_list:
-    for link in news.find_all('a', {'post-link'}):
-        count += 1
-        try:
-
-            response = requests.get(link['href']).content
-            details_soup = BeautifulSoup(response, "html.parser")
-            publish_date, update_date = extract_dates(details_soup, div_class='yazibio')
-            text_content_div = details_soup.find('div', {'class': 'yazi_icerik'})
-            news_content_text = ' '.join(p.get_text(strip = True) for p in text_content_div.find_all('p')) if text_content_div else ''
-            news_data = {
-                "url": link['href'],
-                "header": details_soup.find('h1', {'class': 'single_title'}).get_text(strip=True),
-                "summary": details_soup.find('h2', {'class': 'single_excerpt'}).find('p').get_text(strip=True),
-                "text": news_content_text,
-                "img_url_list": extract_img_urls(details_soup, div_class='post_line'),
-                "publish_date": publish_date,
-                "update_date": update_date
-            }
-            print(news_data)
-            success_count += 1
-        except Exception as e:
-            fail_count += 1
-            print(f"Error occured: {e}")"""
-
 def scrape_article(link):
     try:
         response = requests.get(link).content
@@ -88,6 +86,7 @@ def scrape_article(link):
         publish_date, update_date = extract_dates(details_soup, div_class='yazibio')
         text_content_div = details_soup.find('div', {'class': 'yazi_icerik'})
         news_content_text = ' '.join(p.get_text(strip=True) for p in text_content_div.find_all('p')) if text_content_div else ''
+        logging.info(f"Article scraped successfully from {link}")
         return {
             "url": link,
             "header": details_soup.find('h1', {'class': 'single_title'}).get_text(strip=True),
@@ -98,12 +97,13 @@ def scrape_article(link):
             "update_date": update_date
         }
     except Exception as e:
+        logging.error(f"Error occurred on {link}: {e}")
         print(f"Error occured on {link} : {e}")
         return None
 
 
 all_links = []
-for page_number in range(1, 2):
+for page_number in range(1, max_pages + 1):
     all_links.extend(get_links_from_page(page_number))
 count = len(all_links)
 
@@ -113,6 +113,7 @@ with ThreadPoolExecutor(max_workers = 10) as executor:
         result = future.result()
         if result:
             print(result)
+            """insert_news(result)"""
             success_count += 1
         else:
             fail_count += 1
@@ -130,5 +131,9 @@ stats = {
     "success_count": success_count,
     "fail_count": fail_count
 }
-
+logging.info(f"Finished scraping. Total time: {elapsed_time} seconds.")
+logging.info(f"Scraping stats: {stats}")
+"""insert_stats(stats)"""
 print(stats)
+
+"""client.close()"""
