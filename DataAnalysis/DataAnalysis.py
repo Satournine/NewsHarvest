687fabac821b2ae4d386074a14996a3d0632111c
Satournine <tunaorkun@outlook.com>
2023-12-04 04:30:42 +0300

    analysis_added

---
+++ DataAnalysis/DataAnalysis.py
@@ -0,0 +1,70 @@
+from pymongo import MongoClient
+from collections import Counter
+import re
+import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+import seaborn as sns
+
+
+def get_news_articles(collection):
+    return collection.find({}, {'text': 1, '_id': 0})
+
+
+def get_extra_stopwords(filepath):
+    extra_stop = []
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            extra_stop.append(line.strip())
+    return extra_stop
+
+
+def filter_words(news_articles, stop_words, extra_stop):
+    words = []
+    for data in news_articles:
+        article_words = re.findall(r'\w+', data['text'].lower())
+        filtered_words = [word for word in article_words if word not in stop_words]
+        final_words = [word for word in filtered_words if word not in extra_stop]
+        words.extend(final_words)
+    return words
+
+
+def insert_db(collection, word_frequencies):
+    for word, count in word_frequencies:
+        word_data = {"word": word, "count": count}
+        collection.insert_one(word_data)
+
+
+def plot_word_frequency(word_frequencies):
+    words, counts = zip(*word_frequencies)
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=list(counts), y=list(words))
+
+    plt.xlabel('Frequency')
+    plt.ylabel('Words')
+    plt.title('Top 10 Most Common Words')
+    plt.show()
+
+
+def main():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["orkun_tuna"]
+    news_collection = db["news"]
+    word_frequency_collection = db["word_frequency"]
+
+    turkish_stopwords = stopwords.words('turkish')
+    extra_stopwords = get_extra_stopwords('stopwords.txt')
+    news_articles = get_news_articles(news_collection)
+
+    words = filter_words(news_articles, turkish_stopwords, extra_stopwords)
+    word_counts = Counter(words)
+    most_common_words = word_counts.most_common(10)
+    print(most_common_words)
+
+    insert_db(word_frequency_collection, most_common_words)
+    plot_word_frequency(most_common_words)
+
+
+if __name__ == "__main__":
+    main()
--- DataHarvest/DataHarvest.py
+++ DataHarvest/DataHarvest.py
@@ -15,13 +15,12 @@ os.makedirs(log_directory, exist_ok=True)
 log_filename = "logs.log"
 log_file_path = os.path.join(log_directory, log_filename)
 
-logging.basicConfig(filename=log_file_path, level = logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["orkun_tuna"]
 
 news_collection = db["news"]
-word_frequency_collection = db["word_frequency"]
 stats_collection = db["stats"]
 
 chrome_driver_path = r"C:\Program Files\Google\Chrome\Application\chromedriver.exe"
@@ -36,12 +35,14 @@ browser.get(base_URL)
 
 start_time = time.time()
 
+
 def insert_news(news_data):
     try:
         news_collection.insert_one(news_data)
         logging.info(f"News data inserted successfully: {news_data['url']}")
     except Exception as e:
         logging.error(f"Error inserting news data: {e}")
+
 
 def insert_stats(stats_data):
     try:
@@ -50,15 +51,16 @@ def insert_stats(stats_data):
     except Exception as e:
         logging.error(f"Error inserting stat data: {e}")
 
-def get_links_from_page(page_number):
-    page_url = base_URL if page_number == 1 else f"{base_URL}page/{page_number}/"
+
+def get_links_from_page(pagination):
+    page_url = base_URL if pagination == 1 else f"{base_URL}page/{pagination}/"
     request = requests.get(page_url).content
     soup = BeautifulSoup(request, "html.parser")
     news_list = soup.find_all('div', {'class': 'kategori_yazilist'})
     return [link['href'] for news in news_list for link in news.find_all('a', {'class': 'post-link'})]
 
 
-def extract_img_urls(soup, div_class ):
+def extract_img_urls(soup, div_class):
     div = soup.find('div', {'class': div_class})
     img_urls = []
     if div:
@@ -68,8 +70,9 @@ def extract_img_urls(soup, div_class ):
                 img_urls.append(img_url)
     return img_urls
 
+
 def extract_dates(soup, div_class):
-    yazibio_div = soup.find('div', class_= div_class)
+    yazibio_div = soup.find('div', class_=div_class)
     tarih_spans = yazibio_div.find_all('span', class_='tarih')
 
     publish_date = 'Unknown'
@@ -78,13 +81,14 @@ def extract_dates(soup, div_class):
     try:
         publish_date = tarih_spans[0].find('time').get('datetime')
     except IndexError:
-        pass  #keep publish_date as 'Unknown'
+        pass
     try:
         update_date = tarih_spans[1].find('time').get('datetime')
     except IndexError:
-        pass  #keep update_date as 'Unknown'
+        pass
 
     return publish_date, update_date
+
 
 def scrape_article(link):
     try:
@@ -92,7 +96,8 @@ def scrape_article(link):
         details_soup = BeautifulSoup(response, "html.parser")
         publish_date, update_date = extract_dates(details_soup, div_class='yazibio')
         text_content_div = details_soup.find('div', {'class': 'yazi_icerik'})
-        news_content_text = ' '.join(p.get_text(strip=True) for p in text_content_div.find_all('p')) if text_content_div else ''
+        news_content_text = ' '.join(p.get_text(strip=True) for p in text_content_div.find_all('p')) \
+            if text_content_div else ''
         logging.info(f"Article scraped successfully from {link}")
         return {
             "url": link,
@@ -114,7 +119,7 @@ for page_number in range(1, max_pages + 1):
     all_links.extend(get_links_from_page(page_number))
 count = len(all_links)
 
-with ThreadPoolExecutor(max_workers = 15) as executor:
+with ThreadPoolExecutor(max_workers=15) as executor:
     future_to_link = {executor.submit(scrape_article, link): link for link in all_links}
     for future in as_completed(future_to_link):
         result = future.result()
